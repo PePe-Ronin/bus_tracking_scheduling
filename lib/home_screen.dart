413c1c73fd7e918f75dd84ef628776cbf5c8320d
PePe-Ronin <111848497+PePe-Ronin@users.noreply.github.com>
2024-10-29 11:06:50 +0800

    Updated Registration Form

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:bus/welcome_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -9,6 +10,8 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _lastName = TextEditingController();
@@ -17,29 +20,61 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final _gradeLevel = TextEditingController();
   final _section = TextEditingController();
   final _strand = TextEditingController();
+  final _studentContact = TextEditingController();
   final _parentName = TextEditingController();
   final _parentContact = TextEditingController();
   final _studentLatLNG = TextEditingController();
 
   void _register() async {
     try {
-      await _auth.createUserWithEmailAndPassword(
+      // Register user with Firebase Authentication
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
         email: _emailController.text,
         password: _passwordController.text,
       );
+
+      // After successful registration, save additional user details to Firestore
+      await _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'email': _emailController.text,
+        'lastName': _lastName.text,
+        'firstName': _firstName.text,
+        'middleName': _middleName.text,
+        'gradeLevel': _gradeLevel.text,
+        'section': _section.text,
+        'strand': _strand.text,
+        'studentContact': _studentContact,
+        'parentName': _parentName.text,
+        'parentContact': _parentContact.text,
+        'studentLatLNG': _studentLatLNG.text,
+      });
+
+      // Navigate to the Welcome screen
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => WelcomeScreen()),
       );
     } catch (e) {
-      print('Error: $e'); // Handle error
+      print(
+          'Error: $e'); // Handle error (can show a dialog or toast in production)
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(98, 149, 132, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Text(
+            "Set up your account",
+            textAlign: TextAlign.center,
+          ),
+        ),
+        leading: IconButton(
+            onPressed: () {}, icon: Icon(Icons.arrow_back_ios_new_outlined)),
+      ),
+      backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
@@ -48,18 +83,18 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               Image.asset(
                 'assets/bustii.png',
                 height: 200,
-                width: 200, // Set your desired height here
+                width: 200,
               ),
               Container(
                 padding: const EdgeInsets.all(16.0),
                 margin: const EdgeInsets.only(top: 16.0),
                 decoration: BoxDecoration(
-                  color: const Color.fromRGBO(226, 241, 231, 1),
+                  color: Colors.white.withOpacity(0.3),
                   borderRadius: BorderRadius.circular(10),
                   boxShadow: [
                     BoxShadow(
                       color: Colors.black12,
-                      blurRadius: 4,
+                      blurRadius: 10,
                       offset: Offset(0, 2),
                     ),
                   ],
@@ -100,6 +135,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                       decoration: InputDecoration(labelText: 'Strand'),
                     ),
                     TextField(
+                      controller: _studentContact,
+                      decoration:
+                          InputDecoration(labelText: 'Student Contact No.'),
+                    ),
+                    TextField(
                       controller: _parentName,
                       decoration: InputDecoration(labelText: "Parent's Name"),
                     ),
@@ -118,7 +158,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _register,
-                child: Text('Register'),
+                child: Text(
+                  'Register',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
               ),
             ],
           ),
